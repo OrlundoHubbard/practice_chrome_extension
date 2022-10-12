fcf88f06d25df03d6fcaf79aef2d42e4edd8507f
Orlundo Hubbard <orlundokhubbard@gmail.com>
2022-10-11 22:43:29 -0500

    starting contentScript

---
+++ contentScript.js
@@ -0,0 +1,25 @@
+(() => {
+  let youtubeLeftControls, youtubePlayer;
+  let currentVideo = "";
+  let currentVideoBookmarks = [];
+
+  chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    const { type, value, videoId } = obj;
+
+    if (type === "NEW") {
+      currentVideo = videoId;
+      newVideoLoaded();
+    }
+  });
+
+  const newVideoLoaded = () => {
+    const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+    console.log(bookmarkBtnExists);
+
+    if (!bookmarkBtnExists) {
+      const bookmarkBtn = document.createElement("img");
+
+      bookmarkBtn.src
+    }
+  }
+})
